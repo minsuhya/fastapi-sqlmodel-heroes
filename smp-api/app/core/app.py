15fde33c05bb9396dddf5dadae59e0e8e8324d19
rupi <rupi@mmonstar.co.kr>
2024-02-19 15:02:47 +0900

    mysql added

--- smp-api/app/core/app.py
+++ smp-api/app/core/app.py
@@ -2,8 +2,10 @@ from fastapi import FastAPI
 
 from core.mgdb import get_mongodb
 from core.pgdb import create_db_and_tables, engine
+# from core.mydb import create_db_and_tables, engine
 
-app = FastAPI(title="FastAPI + SQLModel + PostgreSQL", version="0.1.0")
+app = FastAPI(title="FastAPI + SQLModel + PostgreSQL(or Mysql)",
+              version="0.1.0")
 
 
 @app.on_event("startup")
